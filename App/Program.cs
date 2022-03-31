f2ae00354d81e86d378eab32dc2337c62eaba31f
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-03-31 17:19:06 +0300

    Add struct User

---
+++ App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+struct User
+{
+    public string firstName;
+    public string lastName;
+    public string patronymic;
+    public DateTime birthday;
+
+    public User(string firstName, string lastName, string patronymic,DateTime birthday)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.birthday = new DateTime(birthday.Ticks);
+    }
+}
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
